65919bddbdd45dea534b64584637e1a93f40df4f
caiomagri <kaioeduardofm@gmail.com>
2020-02-03 20:05:44 -0300

    [IMP] Adicionando model de agendamento, validações e notifications com mongodb

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,19 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
+import FileController from './app/controller/FileController';
+import ProviderController from './app/controller/ProviderController';
+import AppointmentController from './app/controller/AppointmentController';
+import ScheduleController from './app/controller/ScheduleController';
+import NotificationController from './app/controller/NotificationController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -13,5 +21,17 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedules', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
