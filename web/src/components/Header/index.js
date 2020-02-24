1e5efcef381622e87df6780e39bbc46c1262be3c
caiomagri <kaioeduardofm@gmail.com>
2020-02-24 19:10:43 -0300

    [IMP] Adicionando lista de push notifications e atualização de profile

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+
+import Notifications from '~/components/Notifications';
 
 import logo from '~/assets/logo-purple.svg';
 
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
   return (
     <Container>
       <Content>
@@ -14,14 +18,18 @@ export default function Header() {
           <Link to="/">DASHBOARD</Link>
         </nav>
         <aside>
+          <Notifications />
           <Profile>
             <div>
-              <strong>Caio Magri</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu Perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
-              alt="Caio Magri"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
+              alt={profile.name}
             />
           </Profile>
         </aside>
