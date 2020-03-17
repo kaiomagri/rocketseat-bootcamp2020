45e71501e4781c978b8e9e9543389bbc623beee5
caiomagri <kaioeduardofm@gmail.com>
2020-03-17 18:27:05 -0300

    [ADD] Iniciando desenvolvimento da aplicação mobile

---
+++ mobile/src/routes.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import SignIn from '~/pages/SignIn';
+import SignUp from '~/pages/SignUp';
+
+import Dashboard from '~/pages/Dashboard';
+
+const Stack = createStackNavigator();
+const Tabs = createBottomTabNavigator();
+
+export default function createRouter(isSigned = false) {
+  return !isSigned ? (
+    <Stack.Navigator headerMode="none">
+      <Stack.Screen name="SignIn" component={SignIn} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+    </Stack.Navigator>
+  ) : (
+    <Tabs.Navigator
+      tabBarOptions={{
+        activeTintColor: '#FFF',
+        inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+        style: {
+          backgroundColor: '#8d41a8',
+        },
+        keyboardHidesTabBar: true,
+      }}
+    >
+      <Tabs.Screen name="Dashboard" component={Dashboard} />
+    </Tabs.Navigator>
+  );
+}
